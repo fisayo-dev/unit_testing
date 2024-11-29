c2229a88825bee928e6a51625f4e39320aad2508
fisayo-dev <olufisayobadina@gmail.com>
2024-11-29 20:59:24 +0100

    grouped unit tests

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,14 +1,16 @@
 const lib = require("../lib")
 
-test('absolute - should return a positive number if number is positive ', () => {
-    const result = lib.absolute(1)
-    expect(result).toBe(1)
-})
-test('absolute - should return a positive number if number is negative ', () => {
-    const result = lib.absolute(-1)
-    expect(result).toBe(1)
-})
-test('absolute - should return zero if number is zero ', () => {
-    const result = lib.absolute(0)
-    expect(result).toBe(0)
+describe('absolute -', () => {
+    it('should return a positive number if number is positive ', () => {
+        const result = lib.absolute(1)
+        expect(result).toBe(1)
+    })
+    it('should return a positive number if number is negative ', () => {
+        const result = lib.absolute(-1)
+        expect(result).toBe(1)
+    })
+    it('should return zero if number is zero ', () => {
+        const result = lib.absolute(0)
+        expect(result).toBe(0)
+    })
 })
